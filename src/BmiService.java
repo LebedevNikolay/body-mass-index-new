224abd076c2c9752d4097312cef48a900a5370ce
nikolay <nikolay-kmc@mail.ru>
2024-07-01 15:46:18 +0300

    Ispravleno

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,7 @@
 public class BmiService {
-    public double calculate(double weight, double height) {
-        double bmiIndex = weight / Math.pow(height, 2);
-        return (int) bmiIndex;
+    public int calculate(double weight, double height) {
+        double myBodyMassIndex = weight / Math.pow(height, 2);
+        int Index = (int) myBodyMassIndex;
+        return Index;
     }
 }
